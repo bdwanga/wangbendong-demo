064b21909c37bad0ec993ac82a994947705a990f
wbd <1401710845@qq.com>
2019-02-17 15:02:18 +0800

    增加注册中心和数据库配置加密

--- src/main/java/com/primeton/wbd/WangbendongDemoApplication.java
+++ src/main/java/com/primeton/wbd/WangbendongDemoApplication.java
@@ -2,14 +2,18 @@ package com.primeton.wbd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableEurekaClient
 public class WangbendongDemoApplication
 {
     public static void main(String[] args)
     {
+        /**  配置加解密跟秘钥，与配置文件的密文分开放  */
+        //System.setProperty("jasypt.encryptor.password", "test");
         SpringApplication.run(WangbendongDemoApplication.class, args);
     }
 }
